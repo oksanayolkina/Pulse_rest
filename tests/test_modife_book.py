6d011989f6b00ddc6445fac4a06fa15a2f347960
oksanayolkina <oksana.yolkina@gmail.com>
2017-04-14 16:03:24 +0300

    add verification for tests modife_book and modife_role; change func delete for tests modife_book and modife_role @elpiankova

--- tests/test_modife_book.py
+++ tests/test_modife_book.py
@@ -5,7 +5,8 @@ from models.book import Book
 def book(app_b):
     b = Book(title="QA_m", author="Oksana_m")
     app_b.create_object(b)
-    return b
+    yield b
+    app_b.delete_object(b)
 
 def test_modife(app_b, book):
     book_new = Book(id=book.id, title="QA_modife", author="Oksana_modife")
@@ -14,29 +15,23 @@ def test_modife(app_b, book):
     assert response.status_code == 200
     assert response.json() == book_new.get_dict_with_id()
 
-    app_b.delete_object(book)
-    app_b.delete_object(book_new)
-
 def test_modife_empty_title(app_b, book):
     book_new = Book(id=book.id, title="", author="Oksana_modife")
     response = app_b.modife_object(book_new)
     # Verification
     assert response.status_code == 400
-
-    app_b.delete_object(book)
+    assert book_new.get_dict_with_id() not in app_b.get_objects()
 
 def test_modife_empty_author(app_b, book):
     book_new = Book(id=book.id, title="QA_modife", author="")
     response = app_b.modife_object(book_new)
     # Verification
     assert response.status_code == 400
-
-    app_b.delete_object(book)
+    assert book_new.get_dict_with_id() not in app_b.get_objects()
 
 def test_modife_empty(app_b, book):
     book_new = Book(id=book.id, title="", author="")
     response = app_b.modife_object(book_new)
     # Verification
     assert response.status_code == 400
-
-    app_b.delete_object(book)
+    assert book_new.get_dict_with_id() not in app_b.get_objects()
--- tests/test_modife_role.py
+++ tests/test_modife_role.py
@@ -5,7 +5,8 @@ from models.role import Role
 def role(app_r):
     r = Role(name="QA_m", type="Oksana_m", level=10, book=1)
     app_r.create_object(r)
-    return r
+    yield r
+    app_r.delete_object(r)
 
 def test_modife(app_r, role):
     role_new = Role(id=role.id, name="QA_modife", type="Oksana_modife", level=8, book=2)
@@ -19,30 +20,23 @@ def test_modife(app_r, role):
     assert response["level"] == 8
     assert response["book"] == 2
 
-    app_r.delete_object(role_new)
-    app_r.delete_object(role)
-
-
 def test_modife_empty_title(app_r, role):
     role_new = Role(id=role.id, name="", type="Oksana_modife")
     response = app_r.modife_object(role_new)
     # Verification
     assert response.status_code == 400
-
-    app_r.delete_object(role)
+    assert role_new.get_dict_with_id() not in app_r.get_objects()
 
 def test_modife_empty_author(app_r, role):
     role_new = Role(id=role.id, name="QA_modife", type="")
     response = app_r.modife_object(role_new)
     # Verification
     assert response.status_code == 400
-
-    app_r.delete_object(role)
+    assert role_new.get_dict_with_id() not in app_r.get_objects()
 
 def test_modife_empty(app_r, role):
     role_new = Role(id=role.id, name="", type="")
     response = app_r.modife_object(role_new)
     # Verification
     assert response.status_code == 400
-
-    app_r.delete_object(role)
+    assert role_new.get_dict_with_id() not in app_r.get_objects()
